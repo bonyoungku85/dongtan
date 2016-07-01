3dc7ce5a1172b110ae74021bee80ffaa69b39ebd
EC2 Default User <ec2-user@ip-172-31-15-12.ap-northeast-2.compute.internal>
2016-07-01 05:11:44 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dongtan::Application.config.secret_key_base = '2370c51f4459acfa618d4d547811968b7d7f2863ad59b8ac6112b18e5df2fc3702f5d3fb2eb86a24c5f70a63c905287155df83c91561b09d1a2f2ffd6a712467'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dongtan::Application.config.session_store :cookie_store, key: '_dongtan_session'
